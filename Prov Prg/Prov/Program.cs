32ced5619f36c8757dcf42f2e1cfa35f1224f13c
PhilipB20 <70744825+PhilipB20@users.noreply.github.com>
2020-10-05 09:42:52 +0200

    Add files via upload

---
+++ Prov Prg/Prov/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ;
+            Console.WriteLine("WHERE SHOULD WE SHOOT! 1-10?");
+            string Gun;
+            Gun = Console.ReadLine();
+
+            if (Gun == "7")
+            {
+                Console.WriteLine("Perfect Hit!");
+
+            }
+            else if (Gun == "6" || Gun == "8")
+            {
+                Console.WriteLine("Near Miss");
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Total Miss");
+            }
+            Console.ReadLine();
+        }
+    }
+}
